7f570db399890f75106de6126b129bc696113ba8
electro-lite <alexandrosrousos@gmail.com>
2019-07-22 10:54:28 +0100

    Changed contract to work with new Solidity compiler

--- src/main/java/org/web3j/sample/Application.java
+++ src/main/java/org/web3j/sample/Application.java
@@ -1,44 +1,43 @@
 package org.web3j.sample;
 
-import java.math.BigDecimal;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.web3j.crypto.Credentials;
-import org.web3j.crypto.WalletUtils;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.sample.contracts.generated.Greeter;
+
 import org.web3j.tx.Transfer;
 import org.web3j.tx.gas.ContractGasProvider;
 import org.web3j.tx.gas.DefaultGasProvider;
 import org.web3j.utils.Convert;
 import org.web3j.utils.Numeric;
 
+import java.math.BigDecimal;
+
 /**
  * A simple web3j application that demonstrates a number of core features of web3j:
  *
  * <ol>
- *     <li>Connecting to a node on the Ethereum network</li>
- *     <li>Loading an Ethereum wallet file</li>
- *     <li>Sending Ether from one address to another</li>
- *     <li>Deploying a smart contract to the network</li>
- *     <li>Reading a value from the deployed smart contract</li>
- *     <li>Updating a value in the deployed smart contract</li>
- *     <li>Viewing an event logged by the smart contract</li>
+ * <li>Connecting to a node on the Ethereum network</li>
+ * <li>Loading an Ethereum wallet file</li>
+ * <li>Sending Ether from one address to another</li>
+ * <li>Deploying a smart contract to the network</li>
+ * <li>Reading a value from the deployed smart contract</li>
+ * <li>Updating a value in the deployed smart contract</li>
+ * <li>Viewing an event logged by the smart contract</li>
  * </ol>
  *
  * <p>To run this demo, you will need to provide:
  *
  * <ol>
- *     <li>Ethereum client (or node) endpoint. The simplest thing to do is
- *     <a href="https://infura.io/register.html">request a free access token from Infura</a></li>
- *     <li>A wallet file. This can be generated using the web3j
- *     <a href="https://docs.web3j.io/command_line.html">command line tools</a></li>
- *     <li>Some Ether. This can be requested from the
- *     <a href="https://www.rinkeby.io/#faucet">Rinkeby Faucet</a></li>
+ * <li>Ethereum client (or node) endpoint. The simplest thing to do is
+ * <a href="https://infura.io/register.html">request a free access token from Infura</a></li>
+ * <li>A wallet file. This can be generated using the web3j
+ * <a href="https://docs.web3j.io/command_line.html">command line tools</a></li>
+ * <li>Some Ether. This can be requested from the
+ * <a href="https://www.rinkeby.io/#faucet">Rinkeby Faucet</a></li>
  * </ol>
  *
  * <p>For further background information, refer to the project README.
@@ -56,16 +55,14 @@ public class Application {
         // We start by creating a new web3j instance to connect to remote nodes on the network.
         // Note: if using web3j Android, use Web3jFactory.build(...
         Web3j web3j = Web3j.build(new HttpService(
-                "https://rinkeby.infura.io/<your token>"));  // FIXME: Enter your Infura token here;
+        ));  // FIXME: Enter your Infura token here;
         log.info("Connected to Ethereum client version: "
                 + web3j.web3ClientVersion().send().getWeb3ClientVersion());
 
         // We then need to load our Ethereum wallet file
         // FIXME: Generate a new wallet file using the web3j command line tools https://docs.web3j.io/command_line.html
-        Credentials credentials =
-                WalletUtils.loadCredentials(
-                        "<password>",
-                        "/path/to/<walletfile>");
+        Credentials credentials = Credentials.create("1442b2d4391040c8efcdc6fb1abc4841e6edf33c8c7e9f3755586a748bb39de1");
+
         log.info("Credentials loaded");
 
         // FIXME: Request some Ether for the Rinkeby test network at https://www.rinkeby.io/#faucet
@@ -87,7 +84,7 @@ public class Application {
                 credentials,
                 contractGasProvider,
                 "test"
-                ).send();
+        ).send();
 
         String contractAddress = contract.getContractAddress();
         log.info("Smart contract deployed to address " + contractAddress);
