37a40d0fc2fdedeb4285a7140b451d0c9046b204
electro-lite <alexandrosrousos@gmail.com>
2019-08-07 12:13:00 +0100

    Reverted the changes to the Greater.sol file

--- src/main/solidity/greeter/Greeter.sol
+++ src/main/solidity/greeter/Greeter.sol
@@ -1,16 +1,16 @@
-pragma solidity >=0.4.25;
+pragma solidity ^0.4.25;
 
 // Modified Greeter contract. Based on example at https://www.ethereum.org/greeter.
 
 contract Mortal {
     /* Define variable owner of the type address*/
-    address  owner;
+    address owner;
 
     /* this function is executed at initialization and sets the owner of the contract */
     constructor () public { owner = msg.sender; }
 
     /* Function to recover the funds on the contract */
-    function kill() public payable { if (msg.sender == owner) selfdestruct(owner);}
+    function kill() public { if (msg.sender == owner) selfdestruct(owner); }
 }
 
 contract Greeter is Mortal {
@@ -18,23 +18,23 @@ contract Greeter is Mortal {
     string greeting;
 
     /* this runs when the contract is executed */
-    constructor (string memory _greeting) public {
+    constructor (string _greeting) public {
         greeting = _greeting;
     }
 
-    function newGreeting(string memory _greeting) public {
+    function newGreeting(string _greeting) public {
         emit Modified(greeting, _greeting, greeting, _greeting);
         greeting = _greeting;
     }
 
     /* main function */
-    function greet() public view returns (string memory)  {
+    function greet() public constant returns (string)  {
         return greeting;
     }
 
     /* we include indexed events to demonstrate the difference that can be
     captured versus non-indexed */
     event Modified(
-            string indexed oldGreetingIdx, string indexed newGreetingIdx ,
-            string oldGreeting, string newGreeting);
+        string indexed oldGreetingIdx, string indexed newGreetingIdx,
+        string oldGreeting, string newGreeting);
 }
